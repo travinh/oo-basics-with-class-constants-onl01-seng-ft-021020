e6e721dc33131ddec76dd7dd0e39e0044dd77faa
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-20 03:43:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,9 +12,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-    def brand=(brand)
-      @brand = brand
-      BRANDS << brand
-      puts "hello"
-    end
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+    puts "hello"
+  end
 end
