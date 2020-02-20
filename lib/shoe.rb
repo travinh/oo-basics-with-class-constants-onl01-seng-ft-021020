39952552e125fff1c18a236db6499e9f6d670dc6
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-20 03:53:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,15 +5,7 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
-    if BRANDS.length == 0
-      BRANDS << brand
-    else
-      BRANDS.each do |item|
-        if item!=brand
-          BRANDS << brand
-        end 
-      end
-    end
+    if 
   end
 
   def cobble
